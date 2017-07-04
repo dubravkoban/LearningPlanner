bffba8c178da30e93bed531ac86ad429025302ce
dubravkoban <dubravko.ban@gmail.com>
2017-07-04 18:08:27 +0200

    Initial commit

---
+++ LearningPlanner/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using LearningPlanner.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LearningPlanner.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private DbModel _dbModel;
+
+        public HomeController()
+        {
+            _dbModel = new DbModel();
+        }
+
+        public IHttpActionResult Get()
+        {
+            var resultDb = _dbModel.Material.Find(1);
+            return Ok(resultDb);
+        }
+    }
+}
